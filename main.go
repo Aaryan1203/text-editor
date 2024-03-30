ae408b394a7e4aeae89b151bee3c1af54b69757f
aaryan1203 <aaryanjain1203@gmail.com>
2024-03-30 16:19:17 -0400

    created reading a file

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
 
 func main() {
-    fmt.Println("Hello, Go project!")
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: goeditor <filename>")
+        return
+    }
+
+    filename := os.Args[1]
+    file, err := os.Open(filename)
+    if err != nil {
+        fmt.Printf("Error opening file %s: %v\n", filename, err)
+        return
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text()) 
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Printf("Error reading file %s: %v\n", filename, err)
+    }
 }
